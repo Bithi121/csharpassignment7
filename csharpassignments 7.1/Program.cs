807cb1650643cbdfe35e1cbcb90111aa1ebc99d0
Talukder <bithi.talukder@capgemini.com>
2022-12-29 17:56:30 +0530

    Uploaded

---
+++ csharpassignments 7.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System;
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\BITTALUK\Desktop\CsharpAssignment6";
+
+            var dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            var files = Directory.GetFiles(rootPath, "*.txt", SearchOption.AllDirectories);
+
+            Console.WriteLine("Display directories");
+            foreach (string dir in dirs)
+            {
+                var dirInfo = new DirectoryInfo(dir);
+                Console.WriteLine($"{dirInfo.FullName}");
+            }
+            Console.WriteLine("\n--Display all .txt files");
+            foreach (string file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                Console.WriteLine($"{Path.GetFileName(file)} - { fileInfo.Length } bytes");
+            }
+        }
+    }
+}
